3fa831f9a1f9f3e0d83b5fa630372675c954a871
yago-hirokazu <hyag116185@gmail.com>
2025-07-14 23:03:59 +0900

    Add files

---
+++ elpa/better-defaults-20241028.4/better-defaults-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "better-defaults" "20241028.4"
+  "Fixing weird quirks and poor defaults."
+  '((emacs "25.1"))
+  :url "https://github.com/technomancy/better-defaults"
+  :commit "fde8d8f9cf8c038c8ef902d939df4745e88fac88"
+  :revdesc "fde8d8f9cf8c"
+  :keywords '("convenience"))
